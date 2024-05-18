a853a7cca5df8f361e2126d9caffc58ad8ca9dce
Ayaan <94982193+incognito-root@users.noreply.github.com>
2024-05-18 21:55:31 +0500

    made model change

--- src/main/java/com/tablemasteradmin/admintablemaster/model/MenuItemModel.java
+++ src/main/java/com/tablemasteradmin/admintablemaster/model/MenuItemModel.java
@@ -3,6 +3,7 @@ package com.tablemasteradmin.admintablemaster.model;
 import java.time.LocalDateTime;
 
 public class MenuItemModel {
+    private long menuItemId;
     private String menuItemName;
     private double menuItemPrice;
     private String menuItemImageUrl;
@@ -27,7 +28,29 @@ public class MenuItemModel {
         this.menuItemImage = menuItemImage;
     }
 
+    public MenuItemModel(long menuItemId, String menuItemName, double menuItemPrice, String menuItemImageUrl, String menuItemDescription, int fkCategoryId, int menuItemServing, boolean menuItemIsAvailable, int menuItemTotalOrders, LocalDateTime menuItemAddedDate, String menuItemImage) {
+        this.menuItemId = menuItemId;
+        this.menuItemName = menuItemName;
+        this.menuItemPrice = menuItemPrice;
+        this.menuItemImageUrl = menuItemImageUrl;
+        this.menuItemDescription = menuItemDescription;
+        this.fkCategoryId = fkCategoryId;
+        this.menuItemServing = menuItemServing;
+        this.menuItemIsAvailable = menuItemIsAvailable;
+        this.menuItemTotalOrders = menuItemTotalOrders;
+        this.menuItemAddedDate = menuItemAddedDate;
+        this.menuItemImage = menuItemImage;
+    }
+
     public MenuItemModel() {
+    }
+
+    public long getMenuItemId() {
+        return menuItemId;
+    }
+
+    public void setMenuItemId(long menuItemId) {
+        this.menuItemId = menuItemId;
     }
 
     public String getMenuItemName() {
