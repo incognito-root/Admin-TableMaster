66f4413ee305183a3865ab746ee0f62c6358a4a8
Laiba <hellolaiba3@gmail.com>
2024-05-19 17:36:22 +0500

    add and search button functionality added

--- src/main/java/com/tablemasteradmin/admintablemaster/HelloController.java
+++ src/main/java/com/tablemasteradmin/admintablemaster/HelloController.java
@@ -2,13 +2,17 @@ package com.tablemasteradmin.admintablemaster;
 
 import com.tablemasteradmin.admintablemaster.model.MenuItemModel;
 import com.tablemasteradmin.admintablemaster.services.MenuService;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.TilePane;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
@@ -32,7 +36,12 @@ public class HelloController implements Initializable {
     @FXML
     Button searchbutton;
 
+    @FXML
+    TextField searchtextfield;
+
     ArrayList<MenuItemModel> menuItems = new ArrayList<>();
+    Scene scene=null;
+    Stage stage;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -55,6 +64,24 @@ public class HelloController implements Initializable {
             MenuItemController menuItemController = fxmlLoader.getController();
             menuItemController.setData(menuItem.getMenuItemName(), String.valueOf(menuItem.getMenuItemPrice()));
             tiledpane.getChildren().add(anchorPane);
+        }
+    }
+    public void onclickaddbutton(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("addMenuItems.fxml"));
+        scene = new Scene(fxmlLoader.load(), 1200, 720);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void onclicksearchbutton(ActionEvent actionEvent) throws IOException {
+        String source = searchtextfield.getText();
+        for(MenuItemModel menuItem : menuItems) {
+            if(menuItem.getMenuItemName().equalsIgnoreCase(source)) {
+                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("menu-item-card.fxml"));
+                AnchorPane anchorPane = fxmlLoader.load();
+                MenuItemController menuItemController = fxmlLoader.getController();
+                menuItemController.setData(menuItem.getMenuItemName(), String.valueOf(menuItem.getMenuItemPrice()));
+                tiledpane.getChildren().add(anchorPane);
+            }
         }
     }
 }
